796821f5639f6cde13f1352c2e0db51eed3b1c9a
khris-xp <khrisbharmmano@gmail.com>
2023-06-01 11:10:25 +0700

    feat: install dependencies and layout

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { NavBrand, NavHamburger, NavLi, NavUl, Navbar } from 'flowbite-svelte';
+</script>
+
+<Navbar let:hidden let:toggle>
+	<NavBrand href="/">
+		<img src="/shopee-logo.png" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+	</NavBrand>
+	<NavHamburger on:click={toggle} />
+	<NavUl {hidden}>
+		<NavLi href="/" active={true} activeClass="text-orange-500">Home</NavLi>
+		<NavLi href="/login" nonActiveClass="hover:text-orange-500">Login</NavLi>
+	</NavUl>
+</Navbar>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div
+	class="hero min-h-screen"
+	style="background-image: url(https://garage-pak.com/wp-content/uploads/2021/12/Shopping-Day.jpg);"
+>
+	<div class="hero-overlay bg-opacity-60" />
+	<div class="hero-content text-center text-neutral-content">
+		<div class="max-w-md">
+			<h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+			<p class="mb-5">
+				Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+				quasi. In deleniti eaque aut repudiandae et a id nisi.
+			</p>
+			<button class="btn btn-primary">Get Started</button>
+		</div>
+	</div>
+</div>
