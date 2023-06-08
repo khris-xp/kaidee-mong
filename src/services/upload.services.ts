e8f990cd2d71e50cd5f627e037a8f88ddbd1e7ad
khris-xp <khrisbharmmano@gmail.com>
2023-06-08 20:30:50 +0700

    feat: upload video source

--- src/services/upload.services.ts
+++ src/services/upload.services.ts
@@ -12,12 +12,33 @@ export const uploadService = {
 					'content-type': 'multipart/form-data'
 				},
 				onUploadProgress: (progressEvent: AxiosProgressEvent) => {
-					const progress = Math.round((progressEvent.loaded / progressEvent.total!) * 100);
-					onProgress(progressEvent); // Pass the entire progressEvent instead of just the progress value
+					onProgress(progressEvent);
 				}
 			};
 
 			const response: AxiosResponse = await axiosInstance.post('/api/upload', formData, config);
+			return response.data;
+		} catch (error) {
+			const message = (error as Error).message;
+			throw new Error(message);
+		}
+	},
+	uploadVideo: async (file: File, onProgress: (progressEvent: AxiosProgressEvent) => void) => {
+		try {
+			const formData = new FormData();
+			formData.append('file', file);
+
+			const config = {
+				headers: {
+					'content-type': 'multipart/form-data'
+				},
+				onUploadProgress: (progressEvent: AxiosProgressEvent) => {
+					const progress = Math.round((progressEvent.loaded / progressEvent.total!) * 100);
+					onProgress(progressEvent);
+				}
+			};
+
+			const response: AxiosResponse = await axiosInstance.post('/api/upload/video', formData, config);
 			return response.data;
 		} catch (error) {
 			const message = (error as Error).message;
